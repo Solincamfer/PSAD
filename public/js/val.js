057e4406095252220ef2fa77072f8a6dc61669e5
toio <angel.toyo@solincamfer.com>
2016-11-01 17:07:57 -0400

    AlertasInicio
    Sañade el div de alerta al formulario

--- public/js/val.js
+++ public/js/val.js
@@ -1,5 +1,9 @@
-$(function(){
-    $('#close').click(function(event){
-      $('#log')[0].reset();  
-    });
-});
+//$(function(){
+//    $('#close').click(function(event){
+//      $('#log')[0].reset();  
+//    });
+//});
+
+//$('#login').click(function() {
+//  $('#AccesoTrue').toggleClass( "visible" );
+//});
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,7 +8,9 @@
                 <div id="card">
                     <h1 class="title">Panel de Acceso</h1>
                     <form action="/login/verificar" method="" id="log" data-toggle="validator">
-                        <div class="row">
+                        
+                        <div class="alert alert-success alert-dismissible fade in" role="alert" id="AccesoTrue"><p>Credenciales correctas.</p>
+                            </div>
                             <div class="form-group col-md-6 col-md-offset-3">
                                 <label for="user" style="font-size: 20px"><i class="fa fa-user"></i> Usuario:</label>
                                 <input type="text" id="user" name="user" class="form-control" required>
@@ -17,14 +19,21 @@
                                 <label for="pwd" style="font-size: 20px"><i class="fa fa-lock"></i> Contraseña:</label>
                                 <input type="password" id="pwd" name="pwd" class="form-control" required>
                             </div>
-                        </div>
-                        <div class="form-group row" id="f2">
+                        
+                        <div class="form-group row" id="f2">                            
                             <div class="col-md-2 col-md-offset-8" id="log1">
-                                <button type="submit" class="btn btn-success btn-sm" id="login">Iniciar Sesión <i class="fa fa-sign-in"></i></button>
+                                <button type="submit" class="btn btn-success btn-sm" id="login">
+                                Iniciar Sesión 
+                                <i class="fa fa-sign-in"></i>
+                                </button>
                             </div>
                             <div class="col-md-1" id="log2">
-                                <button type="reset" class="btn btn-danger btn-sm" id="close">Cancelar <i class="fa fa-times"></i></button>
+                                <button type="reset" class="btn btn-danger btn-sm" id="close">
+                                Cancelar 
+                                <i class="fa fa-times"></i>
+                                </button>
                             </div>
+                            
                         </div>
                     </form>
                 </div>
