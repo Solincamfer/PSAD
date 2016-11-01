c99fff9baf96d6a7fbbb4b113b2427d35a8feea6
ricardovirguez <ricardo.virguez@solincamfer.com>
2016-11-01 12:18:45 -0400

    Creación del Login
    Se creo el login, y se agregaron layouts, frameworks.

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,12 @@
+<footer class="ftr">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6" id="ftr1">
+                <p>Copyright <i class="fa fa-copyright"></i> 2016. Todos los derechos reservados.</p>
+            </div>
+            <div class="col-md-6" id="ftr2">
+                <p>Diseñado por: <a href="#">Solincamfer C.A</a></p>
+            </div>
+        </div>
+    </div>
+</footer>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,9 +1,34 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-
-</body>
-</html>
+@extends('admin.base')
+    @section('contenido')
+        @section('title')
+            Login
+        @endsection
+        
+        @include('layout/header')
+            <div class="container">
+                <div id="card">
+                    <h1 class="title">Panel de Acceso</h1>
+                    <form action="" method="post">
+                        <div class="row">
+                            <div class="col-md-6 col-md-offset-3">
+                                <label for="user" style="font-size: 20px"><i class="fa fa-user"></i> Usuario:</label>
+                                <input type="text" id="user" name="user">
+                            </div>
+                            <div class="col-md-6 col-md-offset-3">
+                                <label for="pwd" style="font-size: 20px"><i class="fa fa-lock"></i> Contraseña:</label>
+                                <input type="password" id="pwd" name="pwd">
+                            </div>
+                        </div>
+                        <div class="row" id="f2">
+                            <div class="col-md-2 col-md-offset-8" id="log1">
+                                <button class="btn btn-success btn-sm" id="login">Iniciar Sesión <i class="fa fa-sign-in"></i></button>
+                            </div>
+                            <div class="col-md-1" id="log2">
+                                <button class="btn btn-danger btn-sm" id="close">Cancelar <i class="fa fa-times"></i></button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        @include('layout/footer')
+    @endsection
