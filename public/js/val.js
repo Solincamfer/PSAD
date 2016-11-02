119650b3f93ae66f54df12541f14ec569bcc08b7
ricardovirguez <ricardo.virguez@solincamfer.com>
2016-11-02 10:11:23 -0400

    Validacion Alert
    modificacion

--- public/js/val.js
+++ public/js/val.js
@@ -7,3 +7,7 @@
 //$('#login').click(function() {
 //  $('#AccesoTrue').toggleClass( "visible" );
 //});
+
+$(function(){
+    swal("Lo Siento", "Credenciales Invalidas, Por favor revise nuevamente", "error");  
+});
