7ee6473d922a752223df25ebbeee9c233ad2fa43
ricardovirguez <ricardo.virguez@solincamfer.com>
2016-11-02 12:34:10 -0400

    Inicio de Sesion al Sistema
    Estructura y funcionalidad del sistema una vez se inicia sesion

--- resources/views/redireccion.blade.php
+++ resources/views/redireccion.blade.php
@@ -1,9 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-
-</body>
-</html>
+@extends('admin.base')
+    @section('contenido')
+        @section('title')
+            Inicio
+        @endsection        
+            @include('layout/sidebar')
+                @include('layout/footer')
+    @endsection
