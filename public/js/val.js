a6cafb4748f039aa5cace00b987cbf00d1f9c29e
toio <angel.toyo@solincamfer.com>
2016-11-03 14:04:56 -0400

    Adaptando validacion AJAX,PHP,MYSQL

--- public/js/val.js
+++ public/js/val.js
@@ -27,26 +27,26 @@ $(function(){
                 $(location).attr('href',url);
         });  
     });
-    $( "#log" ).submit(function( event ) {
-	event.preventDefault();
-	var form=$('#log');
-	var url= form.attr('action');
-	var data= form.serialize();
+//     $( "#log" ).submit(function( event ) {
+// 	event.preventDefault();
+// 	var form=$('#log');
+// 	var url= form.attr('action');
+// 	var data= form.serialize();
 
-	var posting = $.post( url, data,function(resultado){
-		if (resultado =='true'){
-			swal('Bienvenido'); 
-		}else{
-			swal('Usuario incorrecto');
-		}		
-	});
-	  posting.fail(function() {
-	    alert( "desconectado" );
-	  })
-	  posting.always(function() {
-	  	//Validar preload
-	    //alert( "complete" );
-	  });
-});
+// 	var posting = $.post( url, data,function(resultado){
+// 		if (resultado =='true'){
+// 			swal('Bienvenido'); 
+// 		}else{
+// 			swal('Usuario incorrecto');
+// 		}		
+// 	});
+// 	  posting.fail(function() {
+// 	    alert( "desconectado" );
+// 	  })
+// 	  posting.always(function() {
+// 	  	//Validar preload
+// 	    //alert( "complete" );
+// 	  });
+// });
 });
 
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,7 +7,7 @@
                 <div id="card">
                     <img src="{{asset('img/PSAD_logo1.png')}}" style="margin-left: 40%; width: 250px;">
                     <!--<h1 class="title">Panel de Acceso</h1>-->
-                    <form action="/login/verificar" method="post" id="log" data-toggle="validator">
+                    <form action="login/verificar/" method="POST" id="log" data-toggle="validator">
                             {{ csrf_field() }}
                             <div class="alert alert-success alert-dismissible fade in" role="alert" id="AccesoTrue"><p>Credenciales correctas.</p>
                             </div>
@@ -22,7 +22,7 @@
                         
                         <div class="form-group row" id="f2">                       
                             <div class="col-md-3 col-md-offset-6" id="log1">
-                                <button type="" class="btn btn-success btn-sm" id="login">
+                                <button type="submit" class="btn btn-success btn-sm" id="login">
                                 Iniciar Sesión 
                                 <i class="fa fa-sign-in"></i>
                                 </button>
@@ -35,7 +35,7 @@
                             </div>
                             <div class="help-block with-errors"></div>
                         </div>
-                        <i class="fa fa-refresh fa-spin login"></i>
+                        <i class="fa fa-refresh fa-spin visible" ></i>
                     </form>
                     <span></span>
                 </div>
