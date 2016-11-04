94de4ba510f8d19887fa02c99d8c4b31fd5eb2da
toio <angel.toyo@solincamfer.com>
2016-11-04 14:33:31 -0400

    mensajes ajax con tiempo de retardo mejorados

--- public/js/AJAX.js
+++ public/js/AJAX.js
@@ -1,19 +1,27 @@
-    
-    $( "#log" ).submit(function( event ) {
+$( "#log" ).submit(function( event ) {
 	event.preventDefault();
 	var form=$('#log');
 	var url= form.attr('action');
 	var data= form.serialize();
-
 	var posting = $.post( url, data,function(resultado){
 		if (resultado == true) {
-			swal('Bienvenido');
-			location.href = "/login/redireccion";
+			swal({
+				title:'Bienvenido',//Contenido del modal
+				timer:2000,//Tiempo de retardo en ejecucion del modal
+				showConfirmButton:false//Eliminar boton de confirmacion
+			});
+			//Retardo en ejecucion de ruta.
+			setTimeout(function(){location.href = "/login/redireccion";},2200); // 3000ms = 3s			
 		}else{
-			swal('Fallo');
+			swal({
+				title:'Credenciales invalidos.',//Contenido del modal
+				timer:1000,//Tiempo de retardo en ejecucion del modal
+				showConfirmButton:false//Eliminar boton de confirmacion
+			});
 		}					
 	});
 	  posting.fail(function() {
+	  	//Ruta al controlador principal no encontrada.
 	    alert( "desconectado" );
 	  })
 	  posting.always(function() {
