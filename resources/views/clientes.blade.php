f535e7bf416ca64892f7a61e94c602a6d324b2d7
ricardovirguez <ricardo.virguez@solincamfer.com>
2016-11-10 12:13:38 -0400

    Arreglo del listado
    Se arreglo el diseño visual de como se reflejan los listados

--- resources/views/clientes.blade.php
+++ resources/views/clientes.blade.php
@@ -1,83 +1,56 @@
 @extends('admin.basesys')
-<style type="text/css">
-	.contenedorModulo{
-		height: 50px; 
-		background: white; 
-		margin-top: 4px; 
-		border-radius: 10px 10px 10px 10px; 
-		border-bottom:7px solid black;
-		border-right:5px solid black; 
-		border-top:1px solid black; 
-		border-left:1px solid black;
-	}
-	.botonAcciones{
-		margin-top:5px; 
-		margin-right:6px; 
-		float: right;
-	}
-	.tituloModulo{
-		margin-top:12px; 
-		margin-left:30px;
-		font-size: 15px;
-	}
-</style>
-<div class="col-xs-5 col-sm-5 col-md-6 col-md-offset-3" style="margin-top: 10em;"> 
-		<button id="botonAgregar" type="button" class="btn" data-toggle="modal" data-target="#myModal">AGREGAR</button>
-		<?php 
-		$filas=5;
-		for ($i=1; $i <=$filas; $i++) {
-
-		 ?>
-		
-		<div class="contenedorModulo" style="" >
-		<?php 
-		$acciones=5;
-		for ($j=1; $j<= $acciones; $j++) { 
-		
-		?>
-			<button class="botonAcciones btn btn-default" type="submit" >Modificar</button>
-		<?php   
-		 } 
-		?>
-			<p class="tituloModulo"><strong>REGISTRO</strong></p>
-		</div>
-		<?php 
-			}
-
-		 ?>
-
-  	</div>
-       		<!-- Modal -->
-			<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-			  <div class="modal-dialog" role="document">
-			    <div class="modal-content">
-			      <div class="modal-header">
-			        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-			        <h4 class="modal-title" id="myModalLabel">Agregar Modulo</h4>
-			      </div>
-			      <div class="modal-body">
-			        <form>
-					  <div class="form-group">
-					    <label for="exampleInputEmail1">Nombre del modulo:</label>
-					    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Modulo.....">
-					  </div>
-					  <div class="form-group">
-					    <label for="exampleInputPassword1">URL modulo padre:</label>
-					    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="URL......">
-					  </div>
-					  <div class="form-group">
-					  <label for="exampleselect">Seleccione estatus:</label>
-					  	<select class="form-control" id="exampleselect">
-						  <option>Activo</option>
-						  <option>Desactivo</option>						  
-						</select>
+	@section('contenido')
+		@section('title')
+			Registro Cliente
+		@endsection        
+			@include('layout/header')
+				@include('layout/sidebar')
+			<div class="contenido">
+				<div class="col-xs-5 col-sm-5 col-md-6 col-md-offset-3" style=""> 
+					<button id="btnAdd" type="button" class="btnAd col-md-offset-10" data-toggle="modal" data-target="#myModal" href="#myModal">AGREGAR <i class="fa fa-plus-circle"></i></button>
+					@for($i=0; $i < 5; $i++)
+					<div class="contMd" style="">
+						@for($j=0; $j < 5; $j++)
+						<button class="btnAcc" type="submit">Modificar</button>
+						@endfor
+						<p class="ttlMd"><strong>REGISTRO</strong></p>
 					</div>
-					</form>
-			      </div>
-			      <div class="modal-footer">
-			        <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
-			        <button type="button" class="btn btn-primary">Guardar</button>
-			      </div>
-			    </div>
-			  </div>
+					@endfor
+				</div>
+				<!-- Modal -->
+				<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+					<div class="modal-dialog" role="document">
+						<div class="modal-content">
+							<div class="modal-header">
+								<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+								<h4 class="modal-title" id="myModalLabel">Agregar Modulo</h4>
+							</div>
+							<div class="modal-body">
+								<form>
+									<div class="form-group">
+										<label for="exampleInputEmail1">Nombre del modulo:</label>
+										<input type="text" class="form-control" id="exampleInputEmail1" placeholder="Modulo.....">
+									</div>
+									<div class="form-group">
+										<label for="exampleInputPassword1">URL modulo padre:</label>
+										<input type="text" class="form-control" id="exampleInputPassword1" placeholder="URL......">
+									</div>
+									<div class="form-group">
+										<label for="exampleselect">Seleccione estatus:</label>
+										<select class="form-control" id="exampleselect">
+											<option>Activo</option>
+											<option>Desactivo</option>                        
+										</select>
+									</div>
+								</form>
+							</div>
+							<div class="modal-footer">
+								<button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+								<button type="button" class="btn btn-primary">Guardar</button>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
+	@endsection
+
