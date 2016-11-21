8e7668d4f800855c44a00aef50cd6613e9306e0c
ricardovirguez <ricardo.virguez@solincamfer.com>
2016-11-21 17:18:44 -0400

    arreglo del checkbox cliente

--- resources/views/Registros_Basicos/Clientes/clientes.blade.php
+++ resources/views/Registros_Basicos/Clientes/clientes.blade.php
@@ -39,11 +39,11 @@
 										@elseif($accion->descripcion=="Status")
 											@if($accion->status_ac==1)
 												<div class="chbx">
-													<input type="checkbox" class="btnAcc" name="status" id="inchbx1" value="{{$accion->status_ac}}" checked><label for="inchbx1" class="tltpck" data-ttl="{{$accion->descripcion}}"></label>
+													<input type="checkbox" class="btnAcc" name="inchbx1" id="{{'inchbx'. $clientes->id}}" value="{{$accion->status_ac}}" checked><label for="{{'inchbx'. $clientes->id}}" class="tltpck" data-ttl="{{$accion->descripcion}}"></label>
 												</div>
 											@elseif($accion->staus_ac==0)
 												<div class="chbx">
-													<input type="checkbox" class="btnAcc" name="status" id="inchbx2" value="{{$accion->status_ac}}"><label for="inchbx2" class="tltpck" data-ttl="{{$accion->descripcion}}"></label>
+													<input type="checkbox" class="btnAcc" name="status" id="{{'inchbx'. $clientes->id}}" value="{{$accion->status_ac}}"><label for="{{'inchbx'. $clientes->id}}" class="tltpck" data-ttl="{{$accion->descripcion}}"></label>
 												</div>
 											@endif
 										@endif
