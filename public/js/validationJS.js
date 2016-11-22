33c37c0db1f385be6200910b817976d8eaf05891
toio <angel.toyo@solincamfer.com>
2016-11-22 17:29:16 -0400

    boton submit

--- public/js/validationJS.js
+++ public/js/validationJS.js
@@ -1,41 +1,4 @@
 $(document).ready(function() {
-    $('#btnSv').click(function(){
-        alert(switchtab);
-var switchtab=0;
-var anterior=0;
-for (var i = 1; i <= 20; i++) {
-        var input= $('#input'+i).val();
-        if (input!="") {
-            switchtab=switchtab+1;
-            alert(switchtab);
-        }else{
-        }        
-    }
-if (switchtab>=5 && switchtab<10) { 
-    $( "#btnSv" ).attr( "type","button");    
-    var backtab=2;
-    $("#a2").click();
-}else if (switchtab>=10 && switchtab<15) {
-    $( "#btnSv" ).attr( "type","button"); 
-    var backtab=3;
-    $("#a3").click();    
-}else if (switchtab>=15 && switchtab<20) {
-    var backtab=4;
-    $( "#btnSv" ).attr( "type","button"); 
-    $( "#btnSv" ).html('Guardar<i class="fa fa-floppy-o"></i>');
-    $("#a4").click();   
-}else if (switchtab==20) { 
-$( "#btnSv" ).attr( "type","submit");  
-    var backtab=5; 
-}
-$('#btnAn').click(function(){        
-    
-    backtab=backtab-1;
-    $( "#btnSv" ).html('Siguiente<i class="fa fa-hand-o-right"></i>');
-        $("#a"+backtab).click();
-    });
-
-});
 
  $('#btnMdSv').click(function(){
     Normal(this);
