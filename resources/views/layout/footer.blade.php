b51e648538c987e07ebce09c4803ef9d09476af2
Vincen Yoel Santaella <vinrast@gmail.com>
2016-11-24 14:32:31 -0430

    cambiando footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="col-xs-12 col-sm-12 col-md-12" id="ftr">
         <div class="row">
             <div class="col-xs-8 col-xs-offset-2 col-md-8 col-md-offset-4">
-                <p>Copyright <i class="fa fa-copyright"></i> 2016. Todos los derechos reservados. | Desarrollado por: <a href="https://www.solincamfer.com/">Solincamfer C.A</a></p>
+                <p>Copyright <i class="fa fa-copyright"></i> 2016. Todos los derechos reservados. | Desarrollado por: <a href="http://www.solincamfer.com/" target="_blank">Solincamfer C.A</a></p>
             </div>
         </div>
     </div>
