8f75ee9870db6895fb7b660ac8a99a884135c48b
TOYO <angel.toyo@solincamfer.com>
2016-11-28 14:35:01 -0400

    Controlar action form cliente mod

--- public/js/AJAX.js
+++ public/js/AJAX.js
@@ -1,9 +1,17 @@
-$( "#log" ).submit(function( event ) {
+
+
+/////Desavilitar accion="" para formularios submit
+$( "#log" ).submit(function( event ){
 	event.preventDefault();
 	});
-$( "#Formcliente" ).submit(function( event ) {
+$( "#Formcliente" ).submit(function( event ){
 	event.preventDefault();
 	});
+$( "#Formclientemd" ).submit(function( event ){
+	event.preventDefault();
+	});
+////////////////////
+////Validacion + permisologia + AJAX del boton submit de la vista LOGIN////
 $('#log1').click(function(){
 	var user = $('#user').val();
 	var pwd = $('#pwd').val();
@@ -14,6 +22,7 @@ $('#log1').click(function(){
 			var data= form.serialize();
 			var posting = $.post( url, data,function(resultado){
 				if (resultado[0] == true) {
+					//SWALLLL mensjes de alerta y sucesos
 					swal({
 						title:'Bienvenido',//Contenido del modal
 						text: '<p style="font-size: 2em;">'+resultado[1]+' '+resultado[2]+'</p>',
@@ -30,7 +39,6 @@ $('#log1').click(function(){
 						timer:1500,//Tiempo de retardo en ejecucion del modal
 						type: "error",
 						showConfirmButton:false//Eliminar boton de confirmacion
-
 					});
 				}					
 			});
@@ -48,14 +56,12 @@ $('#log1').click(function(){
 				//Validar preload
 				//alert( "complete" );
 			});	
-		
-	}else{
-		
+	}else{	
 	}
 });
-
-
-
+/////////////////////////
+///////COMBOS DEPENDIENTES vista cliente
+//Direccion comercial
 $("#inn1").change(function(){
 	$("#inn1 option:selected").each(function () {
 			$( ".limpiarnn0" ).remove();	
@@ -72,6 +78,7 @@ $("#inn1").change(function(){
             var vector=[name,elegido];
             $.get("/menu/registros/clientes/registrar", { vector: vector }, function(data){
             	$.each(data, function(i, item) {
+            		///////////AGREGAR OPCION SEGUN CANTIDAD DE VALORES HABILITADOS/////////////	
             		$('#inn2').append('<option class="limpiarnn0" value="'+item.id+'">'+item.descripcion+'</option>');
 				})        
             });            
@@ -90,13 +97,12 @@ $("#inn2").change(function(){
             var vector=[name,elegido];
             $.get("/menu/registros/clientes/registrar", { vector: vector }, function(data){
             	$.each(data, function(i, item) {
+            		///////////AGREGAR OPCION SEGUN CANTIDAD DE VALORES HABILITADOS/////////////	
             		$('#inn3').append('<option class="limpiarnn1" value="'+item.id+'">'+item.descripcion+'</option>');
 				})        
             });            
-        });
-		
-		});
-
+        });		
+});
 $("#inn3").change(function(){
 	$("#inn3 option:selected").each(function () {
 			$( ".limpiarnn2" ).remove();	
@@ -107,16 +113,14 @@ $("#inn3").change(function(){
             var vector=[name,elegido];
             $.get("/menu/registros/clientes/registrar", { vector: vector }, function(data){
             	$.each(data, function(i, item) {
+            		///////////AGREGAR OPCION SEGUN CANTIDAD DE VALORES HABILITADOS/////////////	
             		$('#inn4').append('<option class="limpiarnnn2" value="'+item.id+'">'+item.descripcion+'</option>');
 				})        
             });            
         });
-	$( ".limpiarnn2" ).remove();
-			
-		});
-
+	$( ".limpiarnn2" ).remove();			
+});
 $("#innn11").change(function(){
-
 	$("#innn11 option:selected").each(function () {
 			$( ".limpiarnnn0" ).remove();	
 			$('#innn12 option:selected').val(0);  
@@ -132,14 +136,13 @@ $("#innn11").change(function(){
             var vector=[name,elegido];
             $.get("/menu/registros/clientes/registrar", { vector: vector }, function(data){
             	$.each(data, function(i, item) {
+            		///////////AGREGAR OPCION SEGUN CANTIDAD DE VALORES HABILITADOS/////////////	
             		$('#innn12').append('<option class="limpiarnnn0" value="'+item.id+'">'+item.descripcion+'</option>');
 				})        
             });            
         });
-	$( ".limpiarnnn" ).remove();
-			
+	$( ".limpiarnnn" ).remove();			
 		});
-
 $("#innn12").change(function(){
 	$("#innn12 option:selected").each(function () {	
 			$( ".limpiarnnn1" ).remove();	
@@ -153,14 +156,13 @@ $("#innn12").change(function(){
             var vector=[name,elegido];
             $.get("/menu/registros/clientes/registrar", { vector: vector }, function(data){
             	$.each(data, function(i, item) {
+            		///////////AGREGAR OPCION SEGUN CANTIDAD DE VALORES HABILITADOS/////////////	
             		$('#innn13').append('<option class="limpiarnnn1" value="'+item.id+'">'+item.descripcion+'</option>');
 				})        
             });            
         });
-	$( ".limpiarnnn1" ).remove();
-			
-		});
-
+	$( ".limpiarnnn1" ).remove();			
+});
 $("#innn13").change(function(){
 	$("#innn13 option:selected").each(function () {	
 	        $( ".limpiarnnn2" ).remove();	
@@ -171,14 +173,13 @@ $("#innn13").change(function(){
             var vector=[name,elegido];
             $.get("/menu/registros/clientes/registrar", { vector: vector }, function(data){
             	$.each(data, function(i, item) {
+            		///////////AGREGAR OPCION SEGUN CANTIDAD DE VALORES HABILITADOS/////////////	
             		$('#innn14').append('<option class="limpiarnnn2" value="'+item.id+'">'+item.descripcion+'</option>');
 				})        
             });            
         });
-	$( ".limpiarnnn2" ).remove();
-			
-		});
-
+	$( ".limpiarnnn2" ).remove();			
+});
 $("#ipp1").change(function(){
 	$("#ipp1 option:selected").each(function () {			
     		var name=$('#ipp1').attr("name");
@@ -186,14 +187,13 @@ $("#ipp1").change(function(){
             var vector=[name,elegido];
             $.get("/menu/registros/clientes/registrar", { vector: vector }, function(data){
             	$.each(data, function(i, item) {
+            		///////////AGREGAR OPCION SEGUN CANTIDAD DE VALORES HABILITADOS/////////////	
             		$('#ipp2').append('<option class="limpiarpp" value="'+item.id+'">'+item.descripcion+'</option>');
 				})        
             });            
         });
-	$( ".limpiarpp" ).remove();
-			
-		});
-
+	$( ".limpiarpp" ).remove();			
+});
 $("#ipp2").change(function(){
 	$("#ipp2 option:selected").each(function () {			
     		var name=$('#ipp2').attr("name");
@@ -201,14 +201,13 @@ $("#ipp2").change(function(){
             var vector=[name,elegido];
             $.get("/menu/registros/clientes/registrar", { vector: vector }, function(data){
             	$.each(data, function(i, item) {
+            		///////////AGREGAR OPCION SEGUN CANTIDAD DE VALORES HABILITADOS/////////////	
             		$('#ipp3').append('<option class="limpiarpp1" value="'+item.id+'">'+item.descripcion+'</option>');
 				})        
             });            
         });
-	$( ".limpiarpp1" ).remove();
-			
-		});
-
+	$( ".limpiarpp1" ).remove();			
+});
 $("#ipp3").change(function(){
 	$("#ipp3 option:selected").each(function () {			
     		var name=$('#ipp3').attr("name");
@@ -216,17 +215,13 @@ $("#ipp3").change(function(){
             var vector=[name,elegido];
             $.get("/menu/registros/clientes/registrar", { vector: vector }, function(data){
             	$.each(data, function(i, item) {
+            		///////////AGREGAR OPCION SEGUN CANTIDAD DE VALORES HABILITADOS/////////////	
             		$('#ipp4').append('<option class="limpiarpp2" value="'+item.id+'">'+item.descripcion+'</option>');
 				})        
             });            
         });
-	$( ".limpiarpp2" ).remove();
-			
-		});
-
-
-
-
+	$( ".limpiarpp2" ).remove();			
+});
 $("#ippp1").change(function(){
 	$("#ippp1 option:selected").each(function () {			
     		var name=$('#ippp1').attr("name");
@@ -234,14 +229,13 @@ $("#ippp1").change(function(){
             var vector=[name,elegido];
             $.get("/menu/registros/clientes/registrar", { vector: vector }, function(data){
             	$.each(data, function(i, item) {
+            		///////////AGREGAR OPCION SEGUN CANTIDAD DE VALORES HABILITADOS/////////////	
             		$('#ippp2').append('<option class="limpiarppp" value="'+item.id+'">'+item.descripcion+'</option>');
 				})        
             });            
         });
-	$( ".limpiarppp" ).remove();
-			
-		});
-
+	$( ".limpiarppp" ).remove();			
+});
 $("#ippp2").change(function(){
 	$("#ippp2 option:selected").each(function () {			
     		var name=$('#ippp2').attr("name");
@@ -249,14 +243,13 @@ $("#ippp2").change(function(){
             var vector=[name,elegido];
             $.get("/menu/registros/clientes/registrar", { vector: vector }, function(data){
             	$.each(data, function(i, item) {
+            		///////////AGREGAR OPCION SEGUN CANTIDAD DE VALORES HABILITADOS/////////////	
             		$('#ippp3').append('<option class="limpiarppp1" value="'+item.id+'">'+item.descripcion+'</option>');
 				})        
             });            
         });
-	$( ".limpiarppp1" ).remove();
-			
-		});
-
+	$( ".limpiarppp1" ).remove();			
+});
 $("#ippp3").change(function(){
 	$("#ippp3 option:selected").each(function () {			
     		var name=$('#ippp3').attr("name");
@@ -264,21 +257,24 @@ $("#ippp3").change(function(){
             var vector=[name,elegido];
             $.get("/menu/registros/clientes/registrar", { vector: vector }, function(data){
             	$.each(data, function(i, item) {
+            		///////////AGREGAR OPCION SEGUN CANTIDAD DE VALORES HABILITADOS/////////////	
             		$('#ippp4').append('<option class="limpiarppp2" value="'+item.id+'">'+item.descripcion+'</option>');
 				})        
             });            
         });
-	$( ".limpiarppp2" ).remove();
-			
-		});
-
-	
+	$( ".limpiarppp2" ).remove();			
+});
+/////////////////////////////////
+///////////LLENADO DEL MODAL MODIFICAR REGISTRO MEDIANTE SU BOTON SUBMIT/////////////	
 	$(".tltp").click(function(){
+		///////////BUSCADO BOTON CLICKEADO/////////////	
 			ID = $(this).attr("id");			
 			idCliente=$('#idCliente'+ID).val();	
-
+		///////////BUSCANDO CAMPO HIDDEN DEL CLIENTE SELECCIONADO Y ALMACENANDOLO EN UNA VARIABLE/////////////		
 			$('#Clienteid').val(idCliente);			
+		///////////PASANDO VARIABLE ID CLIENTE AL CONTROLADOR Y ESPERANDO DATA COMO RESPUESTA/////////////	
 	        $.get("/menu/registros/clientes/modificar", {idCliente: idCliente}, function(data){
+	    ///////////ASIGNANDO LOS VALORES DEL ARRAY A LOS IMPUT CORRESPONDIENTES DEL MODAL MODIFICAR/////////////	
 	        		$('#in11').val(data[0]);
 	        		$('#in12').val(data[1]);
 	        		$('#in13').val(data[3]);
@@ -290,138 +286,103 @@ $("#ippp3").change(function(){
 	        		$('#innnn12').val(data[7]);
 	        		$('#innnn13').val(data[4]);
 	        		$('#innnn14').val(data[5]);
-
 	        		$('#innnn15').val(data[8]);
 
-
+	    ///////////ASIGNACION AUTOMATICA DE LOS VALORES A LOS COMBO DEPENDIENTES PAISES DEL MODAL MODIFICAR/////////////
 	        		$("#inn1 option[value="+ data[11] +"]").attr("selected",true);
 	        		$("#innn11 option[value="+ data[21] +"]").attr("selected",true);
-	        		// $('#inn2 option:selected').val(data[13]);
-	        		// $('#inn3 option:selected').val(data[15]);
-	        		// $('#inn3 option:selected').html(data[16]);
-	        		// $('#inn4 option:selected').val(data[17]);
-	        		// $('#inn4 option:selected').html(data[18]);
-	        		// $('#inn5 option:selected').val(data[19]);	        		
-	        		// $('#inn5 option:selected').html(data[20]);		
 
 
 
-
+	    ///////////VERIFCAR SI LA VARIABLE PAIS EXISTE EN EL VECTOR/////////////
 	        		if ($(data[11]).empty) {
+	        			///////////RECORRER OPTION DEL COMBO DEPENDIENTE Y REGRESAR VALORES SEGU SELECCIONADO/////////////
 		        			$("#inn1 option:selected").each(function () {			
 				    		var name=$('#inn1').attr("name");
 				            elegido=$(this).val();
 				            var vector=[name,elegido];
+				        ///////////PASANDO VARIABLE Y CARGANDO LISTADO CORRESPONDIENTE A LA SELECCION PREVIA Y ESPERANDO DATA COMO RESPUESTA/////////////	
 				            $.get("/menu/registros/clientes/registrar", { vector: vector }, function(data){
 				            	$.each(data, function(i, item) {
 				            		$('#inn2').append('<option class="limpiarnn0" value="'+item.id+'">'+item.descripcion+'</option>');
 								})        
-				            });      
-
-				        });	  
-
+				            });  
+				        });	
+		        		///////////LIMPIAR COMBOS DEPENDIENTES DE ESTE/////////////
 	        			$('#inn2 option:selected').val(data[13]);  
 	        			$('#inn2 option:selected').html(data[14]); 
-
 	        			$("#inn2 option:selected").each(function () {			
 				    		var name=$('#inn2').attr("name");
 				            elegido=$(this).val();
 				            var vector=[name,elegido];
+				         ///////////PASANDO VARIABLE Y CARGANDO LISTADO CORRESPONDIENTE A LA SELECCION PREVIA Y ESPERANDO DATA COMO RESPUESTA/////////////	
 				            $.get("/menu/registros/clientes/registrar", { vector: vector }, function(data){
 				            	$.each(data, function(i, item) {
 				            		$('#inn3').append('<option class="limpiarnn1" value="'+item.id+'">'+item.descripcion+'</option>');
 								})        
-				            });      
-
+				            });  
 				        });	
-
+				        ///////////LIMPIAR COMBOS DEPENDIENTES DE ESTE/////////////
 				        $('#inn3 option:selected').val(data[15]);  
 	        			$('#inn3 option:selected').html(data[16]); 
-
 	        			$("#inn3 option:selected").each(function () {			
 				    		var name=$('#inn3').attr("name");
 				            elegido=$(this).val();
 				            var vector=[name,elegido];
+				         ///////////PASANDO VARIABLE Y CARGANDO LISTADO CORRESPONDIENTE A LA SELECCION PREVIA Y ESPERANDO DATA COMO RESPUESTA/////////////	
 				            $.get("/menu/registros/clientes/registrar", { vector: vector }, function(data){
 				            	$.each(data, function(i, item) {
 				            		$('#inn4').append('<option class="limpiarnn2" value="'+item.id+'">'+item.descripcion+'</option>');
 								})        
-				            });      
-
+				            });   
 				        });	
-
+				        ///////////LIMPIAR COMBOS DEPENDIENTES DE ESTE/////////////
 				        $('#inn4 option:selected').val(data[15]);  
 	        			$('#inn4 option:selected').html(data[16]); 
 	        		}
-
-
-
 	        		if ($(data[21]).empty) {
 		        			$("#innn11 option:selected").each(function () {			
 				    		var name=$('#innn11').attr("name");
 				            elegido=$(this).val();
 				            var vector=[name,elegido];
-				            $.get("/menu/registros/clientes/registrar", { vector: vector }, function(data){
+				         ///////////PASANDO VARIABLE Y CARGANDO LISTADO CORRESPONDIENTE A LA SELECCION PREVIA Y ESPERANDO DATA COMO RESPUESTA/////////////	
+				           $.get("/menu/registros/clientes/registrar", { vector: vector }, function(data){
 				            	$.each(data, function(i, item) {
 				            		$('#innn12').append('<option class="limpiarnn0" value="'+item.id+'">'+item.descripcion+'</option>');
 								})        
-				            });      
-
-				        });	  
-
+				            });  
+				        });	
 	        			$('#innn12 option:selected').val(data[23]);  
 	        			$('#innn12 option:selected').html(data[24]); 
-
 	        			$("#innn12 option:selected").each(function () {			
 				    		var name=$('#innn12').attr("name");
 				            elegido=$(this).val();
 				            var vector=[name,elegido];
-				            $.get("/menu/registros/clientes/registrar", { vector: vector }, function(data){
+				         ///////////PASANDO VARIABLE Y CARGANDO LISTADO CORRESPONDIENTE A LA SELECCION PREVIA Y ESPERANDO DATA COMO RESPUESTA/////////////	
+				           $.get("/menu/registros/clientes/registrar", { vector: vector }, function(data){
 				            	$.each(data, function(i, item) {
 				            		$('#innn13').append('<option class="limpiarnn1" value="'+item.id+'">'+item.descripcion+'</option>');
 								})        
-				            });      
-
+				            });  
 				        });	
-
 				        $('#innn13 option:selected').val(data[25]);  
-	        			$('#innn13 option:selected').html(data[26]); 
-
+	        			$('#innn13 option:selected').html(data[26]);
 	        			$("#innn13 option:selected").each(function () {			
 				    		var name=$('#innn13').attr("name");
 				            elegido=$(this).val();
 				            var vector=[name,elegido];
-				            $.get("/menu/registros/clientes/registrar", { vector: vector }, function(data){
+				         ///////////PASANDO VARIABLE Y CARGANDO LISTADO CORRESPONDIENTE A LA SELECCION PREVIA Y ESPERANDO DATA COMO RESPUESTA/////////////	
+				          $.get("/menu/registros/clientes/registrar", { vector: vector }, function(data){
 				            	$.each(data, function(i, item) {
 				            		$('#innn14').append('<option class="limpiarnn2" value="'+item.id+'">'+item.descripcion+'</option>');
 								})        
-				            });      
-
+				            });  
 				        });	
-
 				        $('#innn14 option:selected').val(data[27]);  
 	        			$('#innn14 option:selected').html(data[28]); 
 	        		}         
 	        });			
 		});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 	$(".btnAcc").click(function(){
 		swal({
 		  title: "Are you sure?",
@@ -440,7 +401,6 @@ $("#ippp3").change(function(){
     	}else{
     		alert($(this).val());
     	}
-
 });
 
 
--- public/js/validationJS.js
+++ public/js/validationJS.js
@@ -1,11 +1,18 @@
 $(document).ready(function(){ 
 
- $('#btnGuardarCliente').click(function(){      
+
+
+
+////////////////BOTONES VALIDADOS PARA GUARDAR TODOS LOS CAMPOS LLENOS///////////////////////////
+/////idInputForm//REPRESENTACION DE TODOS LOS INPUTS DEL FORMULARIO, DIVIDIDOS POR TABS EL NUMERO ADICIONA UN NUMERO DEL 1 AL N SEGUN EXISTENCIA///////////////////////////    
+////////idpanelsTabs////////NOMBRE DE LOS TABS TOMADO COMO INDICE ..... EL CODIGO ADICIONA UN NUMERO CRECIENTE DEL 0 AL N SEGUN EXISTENCIA///////////////////////////  
+/////////Normal///////LLAMADA A LA FUNCION VALIDADORA DE CAMPOS///////////////////////////  
+
+ $('#btnGuardarCliente').click(function(){  
     var idInputForm=['ip','ipp','ippp','ipppp'];
     var idpanelsTabs='am';
     Normal(this,idInputForm,idpanelsTabs);
  });
-
  $('#btnModificarCliente').click(function(){
     var idInputForm=['in1','inn','innn1','innnn1'];
     var idpanelsTabs='amm';
@@ -46,8 +53,9 @@ $('#btnGuardarResponsable2').click(function(){
     var idpanelsTabs='respp';
     Normal(this,idInputForm,idpanelsTabs);
  });
-
-
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////
 
 function Normal(button,idInputForm,idpanelsTabs){
     var Tabcompletado=0;
@@ -55,12 +63,9 @@ function Normal(button,idInputForm,idpanelsTabs){
     var prueba=false;
     for (var a =0; a<idInputForm.length; a++) {        
     if (CantidadTabs(idInputForm[a])) {
-        //alert('Formulario completo');
-        
-        Tabcompletado=Tabcompletado+1;
-       
-        var next=a+1;
-        
+        //alert('Formulario completo');        
+        Tabcompletado=Tabcompletado+1;       
+        var next=a+1;        
         $("#"+idpanelsTabs+next).click();  
         color=$('#'+idpanelsTabs+a).css('color');
             //alert(color.toString());
@@ -68,37 +73,32 @@ function Normal(button,idInputForm,idpanelsTabs){
                 prueba=true;
             }
                 if (prueba==true){
-
                 }else{
                     if (Tabcompletado===idInputForm.length) { 
                     $(button).attr( "type","submit");            
                     }                       
-                }
-                  
+                }                  
     }else{
-        //alert('Tab '+(a+1)+' incompleto');
-        
+        //alert('Tab '+(a+1)+' incompleto');        
     }
-    //alert(Tabcompletado);
-    
+    //alert(Tabcompletado);    
   }
    if (Tabcompletado===idInputForm.length) {
     $(button).html('Guardar <i class="fa fa-floppy-o"></i>'); 
-  }    
-  
-       
+  }
  }
+
+
 function CantidadTabs(nombreId){  
-    var acumulador=0;
-    var cantidadId=$('a').length;
+    var acumulador=0;    
+    var cantidadId=$('a').length;///////////////////CANTIDAD DE ETIQUETAS A EN LA VISTA////////////////////////
     var idExistentes=0;
     var inputLleno=0;
 
-    for (var i =1; i <= cantidadId; i++) {
-                var cantidadTabs =$('#'+nombreId+i).length;
-                var valorInput=$('#'+nombreId+i).val();
-                acumulador=acumulador+cantidadTabs; 
-                 
+    for (var i =1; i <= cantidadId; i++) {///////////////////RECORRO CANTIDAD DE ETIQUETAS A EN LA VISTA////////////////////////
+                var cantidadTabs =$('#'+nombreId+i).length;///////////////////CONCATENAR VALOR TRAIDO POR EL ARREGLO /////CantidadTabs///////////////////
+                var valorInput=$('#'+nombreId+i).val();///////////////////CAPTURAR CADA TEXT TRAIDO POR EL ARREGLO /////CantidadTabs///////////////////
+                acumulador=acumulador+cantidadTabs;                 
                 if( typeof valorInput !== 'undefined' ) {
                     idExistentes=idExistentes+1;                     
                     if( valorInput !== '' ) {
@@ -107,8 +107,8 @@ function CantidadTabs(nombreId){
                 }   
               }
             var inputsVacios=idExistentes - inputLleno;
-              alert('Campos existentes:'+idExistentes);
-              alert('Campos llenos:'+inputLleno); 
+              //alert('Campos existentes:'+idExistentes);
+              //alert('Campos llenos:'+inputLleno); 
               //alert('Campos vacios:'+inputsVacios); 
             if (inputsVacios===0) {
                  //alert('Formulario completo');
@@ -120,6 +120,7 @@ function CantidadTabs(nombreId){
     }
 
 
+//////////////VALIDACIONES DE INPUT, SELECT, TEXTAREAS, POR CLASES ASIGNADAS//////////////////////////
 $('.Validacion').bootstrapValidator({
         feedbackIcons: {
                 valid: 'glyphicon glyphicon-ok',
@@ -128,56 +129,57 @@ $('.Validacion').bootstrapValidator({
             },
     fields: {
         texts: {
-            // All email fields have .userEmail class
+            // EL SELECTOR ES EL NOMBRE DE LA CLASE CON QUE LLAMARAS ESTA VALIDACION EN HTML///
             selector: '.userEmail',
             err: '#messageContainer',
             validators: {
                 notEmpty: {
+                        ///////VALIDA CAMPO VACIO///////////
                             message: 'Campo vacio.'
                         }
             }
         },
         nombres: {
-            // All email fields have .userEmail class
+            // EL SELECTOR ES EL NOMBRE DE LA CLASE CON QUE LLAMARAS ESTA VALIDACION EN HTML///
             selector: '.usernombres',
             err: '#messageContainer',
             validators: {
-                notEmpty: {
+                notEmpty: {///////VALIDA CAMPO VACIO///////////
                             message: 'Campo vacio.'
                         },
             
-            regexp: {
+            regexp: {///////VALIDA CAMPO SOLO LETRAS///////////
                             regexp: /^[a-zA-Z]+$/,
                             message: 'Solo letras'                            
                         }
                     }
         },
         emails: {
-            // All email fields have .userEmail class
+            // EL SELECTOR ES EL NOMBRE DE LA CLASE CON QUE LLAMARAS ESTA VALIDACION EN HTML///
             selector: '.typeEmail',
             err: '#messageContainer',
             validators: {
-                notEmpty: {
+                notEmpty: {///////VALIDA CAMPO VACIO///////////
                             message: 'Campo vacio.'
                         },
-                emailAddress: {
+                emailAddress: {///////VALIDA CAMPO TIPO EMAIL@///////////
                     message: 'Solo email'
                             }
             }
         },
         Rifnumbers: {
-            // All email fields have .userEmail class
+            // EL SELECTOR ES EL NOMBRE DE LA CLASE CON QUE LLAMARAS ESTA VALIDACION EN HTML///
             selector: '.typeRifNumber',
             err: '#messageContainer',
             validators: {
-                notEmpty: {
+                notEmpty: {///////VALIDA CAMPO VACIO///////////
                             message: 'Campo vacio.'
                         },
-                regexp: {
+                regexp: {///////VALIDA CAMPO SOLO NUMEROS///////////
                             regexp: /^[0-9]+$/,
                             message: 'Solo numeros'                            
                         },
-                stringLength: {
+                stringLength: {///////VALIDA CAMPO CANTIDAD DE CARACTERES///////////
                             min: 10,
                             max: 10,
                             message: '10 numeros.'
@@ -185,18 +187,18 @@ $('.Validacion').bootstrapValidator({
             }
         },
         Tlfnumbers: {
-            // All email fields have .userEmail class
+            // EL SELECTOR ES EL NOMBRE DE LA CLASE CON QUE LLAMARAS ESTA VALIDACION EN HTML///
             selector: '.typeTlfNumber',
             err: '#messageContainer',
             validators: {
-                notEmpty: {
+                notEmpty: {///////VALIDA CAMPO VACIO///////////
                             message: 'Campo vacio.'
                         },
-                regexp: {
+                regexp: {///////VALIDA CAMPO SOLO NUMEROS///////////
                             regexp: /^[0-9]+$/,
                             message: 'Solo numeros'
                         },
-                stringLength: {
+                stringLength: {///////VALIDA CAMPO CANTIDAD DE CARACTERES///////////
                             min: 7,
                             max: 7,
                             message: '7 numeros.'
@@ -204,18 +206,18 @@ $('.Validacion').bootstrapValidator({
             }
         },
         DocumentNumbers: {
-            // All email fields have .userEmail class
+            // EL SELECTOR ES EL NOMBRE DE LA CLASE CON QUE LLAMARAS ESTA VALIDACION EN HTML///
             selector: '.typeCiNumber',
             err: '#messageContainer',
             validators: {
-                notEmpty: {
+                notEmpty: {///////VALIDA CAMPO VACIO///////////
                             message: 'Campo vacio.'
                         },
-                regexp: {
+                regexp: {///////VALIDA CAMPO SOLO NUMEROS///////////
                             regexp: /^[0-9]+$/,
                             message: 'Solo numeros'                            
                         },
-                stringLength: {
+                stringLength: {///////VALIDA CAMPO CANTIDAD DE CARACTERES///////////
                             min: 8,
                             max: 9,
                             message: 'Entre 8 o 9 numeros.'
